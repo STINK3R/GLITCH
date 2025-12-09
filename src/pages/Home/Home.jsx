3852f12d07cfbe7b84fcbdf718458b8fbbdc7b0b
STINK3R <RomanPavlovichWork@yandex.com>
2025-12-10 00:40:32 +0300

    Revert "Feature/auth (#6)"
    This reverts commit 006a33547e34001beda25849149b01465c83ac5e.

--- src/pages/Home/Home.jsx
+++
@@ -1,9 +0,0 @@
-/**
- * Главная страница - редирект на страницу событий
- */
-
-import { Navigate } from "react-router-dom";
-
-export const Home = () => {
-  return <Navigate to="/events" replace />;
-};
